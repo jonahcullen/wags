8e14d9b8baa58fe293e0dd26e39eb4b3d562bfcd
cull0084 <cull0084@umn.edu>
2022-12-23 14:01:54 -0600

    put the exclude nodes back in for the remap branch

--- prep_subs.py
+++ prep_subs.py
@@ -250,6 +250,7 @@ def main():
             f"#SBATCH -e slurm_logs/%j.{v['breed']}_{k}.{job_name}.err\n"
             f"#SBATCH -A {account}\n"
             f"#SBATCH -p {partition}\n"
+            "#SBATCH --exclude cn0229,cn1025,cn0128,cn0134,cn0418,cn0522,cn0346,cn0602,cn0499,cn0241,cn0287,cn0281,cn0275,cn0009\n"
         )             
      
         # job submission body 
