0b8e52826854e5058a4f0990fd0368ef012504a3
Jonah Cullen <jonahcullen@gmail.com>
2024-06-18 16:00:07 -0500

    fixed issue where updated config from known genome saved to incorrect location

--- wags/config_joint.py
+++ wags/config_joint.py
@@ -26,14 +26,10 @@ def main():
     # avaiable within container and do not need
     # to be specified
     prep_dir = os.path.dirname(os.path.realpath(__file__))
-    wags_configs = "pipelines/many_wags/configs"
+    wags_configs = Path(prep_dir).parents[0] / "pipelines/many_wags/configs"
     if ref in refs:
         config_n  = f"{ref}_config.yaml"
-        config = os.path.join(
-            str(Path(prep_dir).parents[0]),
-            wags_configs,
-            config_n
-        )
+        config = str(wags_configs / config_n)
         # load known config
         with open(config) as f:
             doc = yaml.safe_load(f)
@@ -43,10 +39,7 @@ def main():
     else:
         # build config from custom_config.yaml
         config_n = f"{ref}_config.yaml"
-        config = os.path.join(
-            str(Path(prep_dir).parents[0]),
-            os.path.join(wags_configs, "custom_config.yaml"),
-        )
+        config = str(wags_configs / "custom_config.yaml")
         # load known config
         with open(config) as f:
             doc = yaml.safe_load(f)
@@ -59,8 +52,7 @@ def main():
     
     # set directory for config file
     if not args.out:
-       #outdir = os.path.join(os.path.expanduser("~"), f".wags/{species}/{ref}/many_wags")
-        outdir = "pipelines/many_wags/configs"
+        outdir = wags_configs
     else:
         outdir = os.path.realpath(os.path.expanduser(args.out))
     os.makedirs(outdir, exist_ok=True)
@@ -82,17 +74,9 @@ def main():
         bak_config_out = config_out + ".bak"
         shutil.copy(config_out, bak_config_out)
         print(f"backup of the original {config_n} created at {bak_config_out}")
-       #ow = input(f"config already exists at {config_out} - overwrite? [y/n] ")
-       #if ow.lower() == 'y':
     with open(config_out,'w') as f:
         yaml.dump(doc, f, sort_keys=False)
         print(f"{ref}_config.yaml saved to {outdir}")
-       #else:
-       #    print("nothing to do - confirm ref name correct or try again")
-   #else:
-   #    with open(config_out,'w') as f:
-   #        yaml.dump(doc, f, sort_keys=False)
-   #        print(f"{ref}_config.yaml saved to {outdir}")
 
 if __name__ == '__main__':
     
@@ -110,8 +94,6 @@ if __name__ == '__main__':
     
     required.add_argument(
         "-r", "--ref",
-       #default="canfam4",
-       #metavar="",
         default=argparse.SUPPRESS,
         metavar="\b",
         help=textwrap.dedent(f'''\
@@ -137,7 +119,6 @@ if __name__ == '__main__':
     optional.add_argument(
         "-o", "--out",
         metavar="\b",
-       #help="location of config out [default: ~/.wags/SPECIES/REF/many_wags/REF_config.yaml]"
         help=textwrap.dedent("""
             location of config out: for genomes included
             in the container the repository config is updated
@@ -229,22 +210,9 @@ if __name__ == '__main__':
             parser.error(f"--species (-s) required if using ref other than {refs}")
  
     # if non default sif location
-    if sif != os.path.join(os.path.expanduser("~"),".sif/wags.sif"):
-        sif = os.path.realpath(os.path.expanduser(sif))
+   #if sif != os.path.join(os.path.expanduser("~"),".sif/wags.sif"):
+   #    sif = os.path.realpath(os.path.expanduser(sif))
     
-    # confirm image exitst
-   #if os.path.isfile(sif):
-   #    print("wags image found!")
-   #else:
-   #    sif_dir = os.path.join(os.path.expanduser("~"),".sif")
-   #    print(
-   #        f"wags image not found at {os.path.realpath(sif)} -> downloading to {sif_dir}"
-   #    )
-   #    url = "https://s3.msi.umn.edu/wags/wags.sif"
-   #    os.makedirs(sif_dir,exist_ok=True)
-   #    wget.download(url,sif_dir)
-   #    sys.exit("\nrerun without --sif or point to directory containing wags.sif")
-
     main()
         
         
